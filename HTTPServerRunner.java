a0e502e4eced337a7bc1d4fda5bf79aa9e362830
COMLAB1 <COMLAB1@COMLAB.COM>
2024-05-16 16:21:23 +0200

    Full commit

--- HTTPServerRunner.java
+++ HTTPServerRunner.java
@@ -1,9 +1,13 @@
-public class HTTPServerRunner {
+ public class HTTPServerRunner {
 
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java HTTPServerRunner <bindAddress> <bindPort>");
+            System.exit(1);
+        }
 
-        String bindAddress; // Initialize with first commandline program argument
-        int bindPort; // Initialize with second commandline program argument
+        String bindAddress = args[0];
+        int bindPort = Integer.parseInt(args[1]);
 
         SimpleNIOHTTPServer simpleNioHttpServer = new SimpleNIOHTTPServer(bindAddress, bindPort);
         simpleNioHttpServer.run();
